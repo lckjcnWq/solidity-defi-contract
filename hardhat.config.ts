63b934081b6a998bc5e440859bfa2316f07c9f8f
wuquan <17324432872@163.com>
2025-01-17 10:34:05 +0800

    feat 1.create hardhat env
    feat 1.create hardhat env

---
+++ hardhat.config.ts
@@ -0,0 +1,36 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
---
+++ test/AMM.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("AMM", function () {
+  it("should pass", async function () {
+    expect(true).to.be.true;
+  });
+});
